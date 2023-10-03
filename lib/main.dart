6cdc0f8b1c96fe09886fcf5c73c6bf4751882111
Pravish11 <pravish.digumber@umail.uom.ac.mu>
2023-10-03 14:16:07 +0400

    theme settings done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'provider/theme_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +13,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      themeMode: ThemeMode.system,
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      //   useMaterial3: true,
+      // ),
+      theme: MyThemes(context).lightTheme,
+      darkTheme: MyThemes(context).darkTheme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
       debugShowCheckedModeBanner: false,
     );
@@ -43,7 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
---
+++ lib/provider/theme_provider.dart
@@ -0,0 +1,50 @@
+import 'package:expense_management_app/constants/title_size.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyThemes {
+  final BuildContext context; // Add a context parameter
+
+  MyThemes(this.context);
+
+  ThemeData get darkTheme {
+    return ThemeData(
+      scaffoldBackgroundColor: Colors.grey.shade900,
+      colorScheme: const ColorScheme.dark(),
+      primaryColor: Colors.black,
+      appBarTheme: AppBarTheme(
+          centerTitle: true,
+          backgroundColor: Colors.grey.shade900,
+          foregroundColor: Colors.white,
+          titleTextStyle: GoogleFonts.poppins(
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+              fontSize: titleSize)),
+      useMaterial3: true,
+      // textTheme: GoogleFonts.poppinsTextTheme(
+      //   Theme.of(context).textTheme()
+      // ),
+    );
+  }
+
+  ThemeData get lightTheme {
+    return ThemeData(
+      scaffoldBackgroundColor: Colors.white,
+      appBarTheme: AppBarTheme(
+          shadowColor: Colors.black,
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.grey.shade900,
+          titleTextStyle: GoogleFonts.poppins(
+              color: Colors.grey.shade900,
+              fontWeight: FontWeight.w500,
+              fontSize: titleSize)),
+      colorScheme: const ColorScheme.light(),
+      primaryColor: Colors.white,
+      useMaterial3: true,
+      // textTheme: GoogleFonts.poppinsTextTheme(
+      //   Theme.of(context).textTheme,
+      // ),
+    );
+  }
+}
